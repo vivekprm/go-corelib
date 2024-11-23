20a90d889cad76e028ba2a77648c489562b87d0a
Vivek Mishra <v.mishra@f5.com>
2024-11-23 14:42:30 +0530

    added cors middleware

---
+++ middlewares/corsHandler.go
@@ -0,0 +1,30 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+)
+
+type CorsConfig struct {
+	AllowedOrigins []string
+	AllowedMethods []string
+	AllowedHeaders []string
+}
+
+type CorsHandler struct {
+	Next   http.Handler
+	Config CorsConfig
+}
+
+func (ch *CorsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if len(ch.Config.AllowedOrigins) > 0 {
+		w.Header().Add("Access-Control-Allow-Origin", strings.Join(ch.Config.AllowedOrigins, ","))
+	}
+	if len(ch.Config.AllowedHeaders) > 0 {
+		w.Header().Add("Access-Control-Allow-Headers", strings.Join(ch.Config.AllowedHeaders, ","))
+	}
+	if len(ch.Config.AllowedMethods) > 0 {
+		w.Header().Add("Access-Control-Allow-Methods", strings.Join(ch.Config.AllowedMethods, ","))
+	}
+	ch.Next.ServeHTTP(w, r)
+}
